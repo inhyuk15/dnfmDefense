5ace7c6c47fef6808eeed5fbb77ebd9831883a57
inhyuk15 <15inhyuk@gmail.com>
2023-07-17 18:17:37 +0900

    [#5] manager, panel folder, gameobject created

---
+++ Assets/Resources/Scripts/Units/Common/Unit.cs
@@ -0,0 +1,19 @@
+public class Unit
+{
+    public void Movement() {}
+    public void Attack() {}
+
+    private string name;
+
+    private double hp;
+    private double mp;
+    private double range;
+    private double criticalChange;
+    private double manaRegeneration;
+    private double visionRange;
+
+    private double damage;
+    private double armor;
+    private double attackSpeed;
+    private double moveSpeed;
+}
